a6f383d49a0c6b50868878233fb387bc30b50824
emilielimaburke <emilie.burke@gmail.com>
2016-02-20 12:53:09 -0500

    first blogger commit

---
+++ hello_world.rb
@@ -0,0 +1,33 @@
+def bank_balance(amount)
+  if amount < 940
+    puts "You need to transfer money RIGHT NOW!"
+  elsif amount < 1300
+    puts "You need to transfer money ASAP"
+  elsif amount == 1300
+    puts "You should transfer money now"
+  elsif amount < 1500
+    puts "You should transfer money soon"
+  elsif amount <= 2600
+    puts "You're doing okay" 
+  else
+    puts "You're in great shape!"
+  end
+end
+
+
+class Fellow
+  attr_accessor :first_name, :last_name, :primary_phone_number, :company
+
+  def introduction
+    print "#{first_name} works at #{company}"
+    if !primary_phone_number.nil?
+      puts " and can be reached at #{primary_phone_number}."
+    else 
+      puts "."
+    end
+  end
+end
+
+
+
+
